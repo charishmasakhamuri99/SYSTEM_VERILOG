d9d11006ee224723357c6eae70fc7c058d593bfc
S.Charishma <166210153+charishmasakhamuri99@users.noreply.github.com>
2025-09-04 11:17:41 +0530

    Pass by reference constant b tb code

---
+++ R. PASS BY REFERENCE/Pass by reference constant b tb.v
@@ -0,0 +1,19 @@
+// Code your design here
+module tb;
+  task automatic swap( ref bit[3:0] a, const ref bit[3:0]b);
+    bit [3:0] temp;
+    temp=a;
+    a=b;
+   // b=temp;
+    $display("a=%0d,b=%0d",a,b);
+  endtask
+  bit [3:0] a;
+  bit [3:0] b;
+  initial begin
+    a=9;
+    b=7;
+    swap(a,b);
+    #10;
+    $display("a=%0d,b=%0d",a,b);
+  end
+endmodule
