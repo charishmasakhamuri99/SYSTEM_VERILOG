ef190188e552ff5002734bb29e3d7bfd7811f955
S.Charishma <166210153+charishmasakhamuri99@users.noreply.github.com>
2025-09-23 18:35:18 +0530

    super keyword tb

---
+++ Z.7 BASE CLASS OVERRIDING/super keyword tb.v
@@ -0,0 +1,21 @@
+class parent;
+  int a=20;
+  function void display();
+    $display(" a=%0d",a);
+  endfunction
+endclass
+
+class child extends parent;
+  int a=40;
+  function void display();
+    super.display();
+    $display(" a=%0d",a);
+  endfunction
+endclass
+module tb;
+  child c;
+  initial begin
+    c=new();
+    c.display();
+  end
+endmodule
