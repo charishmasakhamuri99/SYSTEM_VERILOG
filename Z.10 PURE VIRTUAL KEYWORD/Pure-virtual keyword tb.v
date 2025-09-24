74290ccb4b5d9111452e06575016c8647fb406f2
S.Charishma <166210153+charishmasakhamuri99@users.noreply.github.com>
2025-09-24 11:49:05 +0530

    Pure-virtual keyword tb

---
+++ Z.10 PURE VIRTUAL KEYWORD/Pure-virtual keyword tb.v
@@ -0,0 +1,28 @@
+// Code your testbench here
+// or browse Examples
+virtual class base;
+  int a;
+  int b;
+  pure virtual function void display();
+    //$display("a=%0d,b=%0d",a,b);
+  //endfunction
+endclass
+    class sub extends base;
+      int a;
+      int b;
+      function void display();
+        $display("a=%0d,b=%0d",a,b);
+      endfunction
+    endclass
+
+module tb;
+  base ba;
+  sub s;
+  initial begin
+    s=new();
+    ba=s;
+    s.a=10;
+    s.b=20;
+    ba.display();
+  end
+endmodule
